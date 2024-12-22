0f26070c4e6f5f7e6df155929d6a9fc9bc8e1183
peyman7585 <abbaszadeh.8575@gmail.com>
2024-12-22 12:46:13 +0330

    add Random card function and images

--- CardWorkout/CardSelectionVC.swift
+++ CardWorkout/CardSelectionVC.swift
@@ -11,24 +11,39 @@ class CardSelectionVC: UIViewController {
 
     @IBOutlet weak var cardImageView: UIImageView!
     @IBOutlet var buttons: [UIButton]!
+    var cards: [UIImage] = Card.allValues
+    var timer: Timer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        startTimer()
         for Button in buttons {
             Button.layer.cornerRadius = 8
         }
+    }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        timer.invalidate()
+    }
+    
+    func startTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(showRandomImage), userInfo: nil, repeats: true)
+    }
+    
+    @objc func showRandomImage() {
+        
+        cardImageView.image = cards.randomElement() ?? UIImage(named: "AS")
     }
     
     @IBAction func stopButtonTapped(_ sender: UIButton) {
+        timer.invalidate()
     }
     
     @IBAction func restartButoonTapped(_ sender: UIButton) {
+        timer.invalidate()
+        startTimer()
     }
     
-    
-    
-    @IBAction func rulesButtonTapped(_ sender: UIButton) {
-    }
 }
